10d2ce5b735c49083c054804d06a5408bb8f05a4
Oleg Dudkin <dudkin.on@gmail.com>
2018-01-03 01:15:26 -0600

    feat: Add cats array

---
+++ js/app.js
@@ -0,0 +1,36 @@
+(function() {
+
+  var Cats = [
+    {
+      clickCount: 0,
+      name: 'Fluffy',
+      image: 'img/22252709_010df3379e_z.jpg',
+      nicknames: ['Kit']
+    },
+    {
+      clickCount: 0,
+      name: 'Sassy',
+      image: 'img/1413379559_412a540d29_z.jpg',
+      nicknames: ['Sassperila']
+    },
+    {
+      clickCount: 0,
+      name: 'Cleopatra',
+      image: 'img/4154543904_6e2428c421_z.jpg',
+      nicknames: ['Cleo']
+    },
+    {
+      clickCount: 0,
+      name: 'Oreo',
+      image: 'img/9648464288_2516b35537_z.jpg',
+      nicknames: ['baby']
+    },
+    {
+      clickCount: 0,
+      name: 'Whiskers',
+      image: 'img/434164568_fea0ad4013_z.jpg',
+      nicknames: ['Whiskeras']
+    }
+  ];
+
+}());
